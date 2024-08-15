f90357d23d7a1f0594188e41adebfb25060837e5
Invokedzz <162361529+Invokedzz@users.noreply.github.com>
2024-08-15 11:59:36 -0300

    adding new features

---
+++ assets/sources.js
@@ -0,0 +1,38 @@
+async function doFetch() {
+    try {
+        const response = await fetch("/toRPG/items.json");
+        
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
+        
+        if (!Array.isArray(data.items)) throw new Error('Expected an array under "items" key');
+        doingtherpg(data.items); // O PROBLEMA TAVA AQUI
+
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+function doingtherpg(items) {
+    const table = document.createElement('table');
+
+    for (let item of items) {
+        let tr = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+        td1.innerHTML = item.name;
+        tr.appendChild(td1);
+
+        let td2 = document.createElement('td');
+        td2.innerHTML = item.type;
+        tr.appendChild(td2);
+
+        table.appendChild(tr);
+    }
+
+    const masterDiv = document.querySelector('.chatblock');
+    masterDiv.appendChild(table);
+}
+
+
+doFetch();
